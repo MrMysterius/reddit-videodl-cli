87b4cc749a33d1a0d969fbf38371fe685a6ee60a
MrMysterius <official.mrmysterius@gmail.com>
2021-05-09 18:00:32 +0200

    Fixed the ffmpeg args again

--- bin/convert-only-video.sh
+++ bin/convert-only-video.sh
@@ -1,1 +1,2 @@
-ffmpeg -i "$1_video.mp4" -c:v libx265 -vbr 2 -crf 28 "$1.webm"
+ffmpeg -i "$1_video.mp4" -c:v libx265 -vbr 3 -crf 28 "$1.mp4"
+ffmpeg -i "$1.mp4" -crf 28 "$1.webm"
--- bin/convert-with-audio.sh
+++ bin/convert-with-audio.sh
@@ -1,1 +1,2 @@
-ffmpeg -i "$1_video.mp4" -i "$1_audio.mp4" -c:v copy -c:v libx265 -vbr 2 -crf 28 "$1.webm"
+ffmpeg -i "$1_video.mp4" -i "$1_audio.mp4" -c:v copy -c:v libx265 -vbr 3 -crf 28 "$1.mp4"
+ffmpeg -i "$1.mp4" -crf 28 "$1.webm"
